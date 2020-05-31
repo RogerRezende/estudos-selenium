9c2f0749d379449961962a07cdb20fd1b2751f8d
Roger Rezende <roger.rezendegmail.com>
2020-05-31 20:18:26 -0300

    fazendo o teste múltiplas vezes

--- preenchendoFormularioLigaTennis.py
+++ preenchendoFormularioLigaTennis.py
@@ -2,60 +2,65 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
-# Criar instância do navegador
-driver = webdriver.Chrome()
+contador = 0
 
-# Abrir a página do cadastro da Liga Vale Tennis Amador
-driver.get('https://ligavaletennis.esp.br/formulario.php')
+while(contador < 5):
+    # Criar instância do navegador
+    driver = webdriver.Chrome()
 
-# Maximizar o browser
-driver.maximize_window()
+    # Abrir a página do cadastro da Liga Vale Tennis Amador
+    driver.get('https://ligavaletennis.esp.br/formulario.php')
 
-# Pegar o campo nome do tenista
-campo_Nome = driver.find_element_by_name('nome')
+    # Maximizar o browser
+    driver.maximize_window()
 
-# Pegar o campo sobrenome do tenista
-campo_Sobrenome = driver.find_element_by_name('sobrenome')
+    # Pegar o campo nome do tenista
+    campo_Nome = driver.find_element_by_name('nome')
 
-# Pegar o campo apelido do tenista
-campo_Apelido = driver.find_element_by_name('apelido')
+    # Pegar o campo sobrenome do tenista
+    campo_Sobrenome = driver.find_element_by_name('sobrenome')
 
-# Pegar o campo e-mail do tenista
-campo_Email = driver.find_element_by_name('email')
+    # Pegar o campo apelido do tenista
+    campo_Apelido = driver.find_element_by_name('apelido')
 
-# Pegar o campo telefone do tenista
-campo_Telefone = driver.find_element_by_name('phone')
+    # Pegar o campo e-mail do tenista
+    campo_Email = driver.find_element_by_name('email')
 
-# Pegar o select categoria do tenista
-select_Categoria = Select(driver.find_element_by_name('categoria'))
+    # Pegar o campo telefone do tenista
+    campo_Telefone = driver.find_element_by_name('phone')
 
-# Pegar o campo senha do tenista
-campo_Senha = driver.find_element_by_name('senha')
+    # Pegar o select categoria do tenista
+    select_Categoria = Select(driver.find_element_by_name('categoria'))
 
-# Pegar o campo confirmar senha do tenista
-campo_ConfirmarSenha = driver.find_element_by_name('confirmarSenha')
+    # Pegar o campo senha do tenista
+    campo_Senha = driver.find_element_by_name('senha')
 
-# Digitar um nome aleatório
-campo_Nome.send_keys('Scott')
+    # Pegar o campo confirmar senha do tenista
+    campo_ConfirmarSenha = driver.find_element_by_name('confirmarSenha')
 
-# Digitar um sobrenome aleatório
-campo_Sobrenome.send_keys('Free')
+    # Digitar um nome aleatório
+    campo_Nome.send_keys('Scott')
 
-# Digitar um apelido aleatório
-campo_Apelido.send_keys('Sr.Milagre')
+    # Digitar um sobrenome aleatório
+    campo_Sobrenome.send_keys('Free')
 
-# Digitar um email aleatório
-campo_Email.send_keys('scottfree@srmilagre.com')
+    # Digitar um apelido aleatório
+    campo_Apelido.send_keys('Sr.Milagre')
 
-# Digitar um telefone aleatório
-campo_Telefone.send_keys('012-34985-3325')
+    # Digitar um email aleatório
+    campo_Email.send_keys('scottfree@srmilagre.com')
 
-# Selecionar a opção classe A
-select_Categoria.select_by_visible_text('Classe A')
+    # Digitar um telefone aleatório
+    campo_Telefone.send_keys('012-34985-3325')
 
-# Digitar uma senha aleatória
-campo_Senha.send_keys('srmilagre')
+    # Selecionar a opção classe A
+    select_Categoria.select_by_visible_text('Classe A')
 
-# Digitar uma confirmação de senha aleatória
-campo_ConfirmarSenha.send_keys('srmilagre')
+    # Digitar uma senha aleatória
+    campo_Senha.send_keys('srmilagre')
+
+    # Digitar uma confirmação de senha aleatória
+    campo_ConfirmarSenha.send_keys('srmilagre')
+
+    contador = contador + 1
 
